607ae937b0133afa077d7f3f30fc1e3f33c52211
Муковский Даниил <br-avm32@yandex.ru>
2023-01-11 18:13:09 +0300

    feat: upload and unzip file

--- internal/handler/zip.go
+++ internal/handler/zip.go
@@ -4,17 +4,23 @@ import (
 	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"io"
+	"log"
+	"mime/multipart"
 	"net/http"
+	"strings"
 	"zip_service/internal/apperror"
 	"zip_service/internal/dto"
 )
 
 const (
 	Download = "/download/"
+	Upload   = "/upload/"
 )
 
 type ZipService interface {
-	StreamAllFiles(files []dto.FileEntry, destination io.Writer) error
+	GetFiles(files []dto.FileEntry, destination io.Writer) error
+	UploadFile(fileName string, part *multipart.Part) (bytesWritten int64, partsWritten int64, err error)
+	UnzipFile(fileName string) (err error)
 }
 
 type zipHandler struct {
@@ -29,6 +35,46 @@ func NewZipHandler(s ZipService) Handler {
 
 func (h *zipHandler) Register(router *httprouter.Router) {
 	router.HandlerFunc(http.MethodPost, Download, apperror.Middleware(h.Download))
+	router.HandlerFunc(http.MethodPost, Upload, apperror.Middleware(h.UploadZIP))
+}
+
+func (h *zipHandler) UploadZIP(w http.ResponseWriter, req *http.Request) error {
+	multipartReader, err := req.MultipartReader()
+	if err != nil {
+		log.Printf("failed to get a multipart reader %v", err)
+		return err
+	}
+
+	partBytes := int64(0)
+	partCount := int64(0)
+	for {
+		part, partErr := multipartReader.NextPart()
+		if partErr != nil {
+			if partErr == io.EOF {
+				break
+			} else {
+				return partErr
+			}
+		} else {
+			if len(part.FileName()) > 0 && strings.HasSuffix(part.FileName(), ".zip") {
+				fileName := "root" + "/" + part.FileName()
+				partBytesIncr, partCountIncr, err := h.service.UploadFile(fileName, part)
+				if err != nil {
+					return err
+				}
+				err = h.service.UnzipFile(fileName)
+				if err != nil {
+					return err
+				}
+				partBytes += partBytesIncr
+				partCount += partCountIncr
+			}
+		}
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	return nil
 }
 
 func (h *zipHandler) Download(w http.ResponseWriter, req *http.Request) error {
@@ -47,7 +93,7 @@ func (h *zipHandler) StreamEntries(zipDescriptor *dto.ZipDescriptor, w http.Resp
 	w.Header().Set("Content-Type", "application/zip")
 	w.Header().Set("Content-Disposition", "attachment; filename=\"archive.zip\"")
 	w.WriteHeader(http.StatusOK)
-	err := h.service.StreamAllFiles(zipDescriptor.Files, w)
+	err := h.service.GetFiles(zipDescriptor.Files, w)
 	if err != nil {
 		// Close the connection so the client gets an error instead of 200 but invalid file
 		closeForError(w)
--- internal/service/zip_streamer.go
+++ internal/service/zip_streamer.go
@@ -2,10 +2,15 @@ package service
 
 import (
 	"archive/zip"
+	"bufio"
+	"fmt"
 	"io"
-	"net/http"
+	"log"
+	"mime/multipart"
 	"os"
 	"path"
+	"path/filepath"
+	"strings"
 	"time"
 	"zip_service/internal/apperror"
 	"zip_service/internal/dto"
@@ -16,13 +21,17 @@ const (
 )
 
 type ZipStreamer struct {
+	bufferSize int
 }
 
 func NewZipStreamer() *ZipStreamer {
-	return &ZipStreamer{}
+	return &ZipStreamer{
+		// 1 KB
+		bufferSize: 8 * 1024,
+	}
 }
 
-func (s *ZipStreamer) StreamAllFiles(files []dto.FileEntry, destination io.Writer) error {
+func (s *ZipStreamer) GetFiles(files []dto.FileEntry, destination io.Writer) error {
 	if len(files) == 0 {
 		return apperror.NewAppError(nil, "must have at least 1 file", "")
 	}
@@ -38,7 +47,7 @@ func (s *ZipStreamer) StreamAllFiles(files []dto.FileEntry, destination io.Write
 
 		header := &zip.FileHeader{
 			Name:     entry.ZipPath,
-			Method:   zip.Store,
+			Method:   zip.Deflate,
 			Modified: time.Now(),
 		}
 
@@ -47,19 +56,30 @@ func (s *ZipStreamer) StreamAllFiles(files []dto.FileEntry, destination io.Write
 			return err
 		}
 
-		_, err = io.Copy(entryWriter, file)
-		if err != nil {
-			return err
-		}
+		var partsWritten = int64(0)
+		var bytesWritten = int64(0)
+		var lastBytesRead = 0
+		buffer := make([]byte, s.bufferSize)
 
-		err = zipWriter.Flush()
-		if err != nil {
-			return err
-		}
+		for lastBytesRead >= 0 {
+			bytesRead, berr := file.Read(buffer)
+			lastBytesRead = bytesRead
+			if berr == io.EOF {
+				break
+			}
+			if berr != nil {
+				log.Printf("error reading data! %v", berr)
+				return berr
+			}
+			if lastBytesRead > 0 {
+				bytesWritten += int64(lastBytesRead)
+				partsWritten++
 
-		flushingWriter, ok := destination.(http.Flusher)
-		if ok {
-			flushingWriter.Flush()
+				_, err = entryWriter.Write(buffer[:bytesRead])
+				if err != nil {
+					return err
+				}
+			}
 		}
 
 		success++
@@ -70,4 +90,97 @@ func (s *ZipStreamer) StreamAllFiles(files []dto.FileEntry, destination io.Write
 	}
 
 	return zipWriter.Close()
+}
+
+func (s *ZipStreamer) UploadFile(fileName string, part *multipart.Part) (bytesWritten int64, partsWritten int64, err error) {
+	log.Printf("read part %s", fileName)
+
+	drainTo, drainErr := os.Create(fileName)
+
+	if drainErr != nil {
+		log.Printf("cannot write out file %s, %v", fileName, drainErr)
+		return 0, 0, drainErr
+	}
+
+	drain := bufio.NewWriter(drainTo)
+
+	var lastBytesRead int
+	buffer := make([]byte, s.bufferSize)
+	for lastBytesRead >= 0 {
+		bytesRead, err := part.Read(buffer)
+		lastBytesRead = bytesRead
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("error reading data! %v", err)
+			return 0, 0, err
+		}
+		if lastBytesRead > 0 {
+			bytesWritten += int64(lastBytesRead)
+			_, err = drain.Write(buffer[:bytesRead])
+			if err != nil {
+				return 0, 0, err
+			}
+			partsWritten++
+		}
+	}
+	err = drain.Flush()
+	if err != nil {
+		return 0, 0, err
+	}
+	log.Printf("wrote file %s of length %d", fileName, bytesWritten)
+
+	return bytesWritten, partsWritten, nil
+}
+
+func (s *ZipStreamer) UnzipFile(pathName string) (err error) {
+	dst := "root/uploads"
+	archive, err := zip.OpenReader(pathName)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = archive.Close()
+	}()
+	for _, f := range archive.File {
+		filePath := filepath.Join(dst, f.Name)
+		fmt.Println("unzipping file ", filePath)
+
+		if !strings.HasPrefix(filePath, filepath.Clean(dst)+string(filepath.Separator)) {
+			fmt.Println("invalid file path")
+			return
+		}
+		if f.FileInfo().IsDir() {
+			fmt.Println("creating directory...")
+			err = os.MkdirAll(filePath, os.ModePerm)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+		if err = os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+			return err
+		}
+
+		dstFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+
+		fileInArchive, err := f.Open()
+		if err != nil {
+			return err
+		}
+
+		if _, err = io.Copy(dstFile, fileInArchive); err != nil {
+			return err
+		}
+
+		err = fileInArchive.Close()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
